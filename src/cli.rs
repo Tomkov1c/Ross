c0cc25e8e06f7c458d072f7baf7fe5fb84f78126
Tomkov1c <tomkliner9@gmail.com>
2026-03-02 10:45:53 +0100

    Move CLI related stuff to a seperate file

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Ross", about = "Code formatter", version, disable_version_flag = true)]
+pub struct Cli {
+
+    #[arg(short = 'v', long, action = clap::ArgAction::Version, help = "Print version information")]
+    pub version: Option<bool>,
+
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Hello { name: Option<String>, },
+    Add { a: f64, b: f64 },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,14 @@
-use clap::{Parser, Subcommand};
+mod cli;
+use cli::{Cli, Commands};
 
-#[derive(Parser)]
-#[command(name = "Ross", about = "My CLI tool", version)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
+use clap::{Parser};
 
-#[derive(Subcommand)]
-enum Commands {
-    /// Say hello
-    Hello {
-        /// Name to greet
-        name: Option<String>,
-    },
-    /// Add two numbers
-    Add { a: f64, b: f64 },
-}
 
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Hello { name } => {
-            println!("Hello, {}!", name.unwrap_or("world".to_string()));
-        }
-        Commands::Add { a, b } => {
-            println!("{} + {} = {}", a, b, a + b);
-        }
+        Commands::Hello { name } => { println!("Hello, {}!", name.unwrap_or("world".to_string())); }
+        Commands::Add { a, b } => { println!("{} + {} = {}", a, b, a + b); }
     }
 }
