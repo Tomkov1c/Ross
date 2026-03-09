42f044778351c6dab6f066c0cf4a7f02cbf89d3f
Tomkov1c <tomkliner9@gmail.com>
2026-03-09 11:06:10 +0100

    Add a function that makes project config folder hidden on windows

--- src/handlers/config_handler.rs
+++ src/handlers/config_handler.rs
@@ -1,17 +1,38 @@
 use std::path::PathBuf;
 
-fn get_or_create_config_dir() -> PathBuf {
+pub fn get_or_create_config_dir() -> PathBuf {
     let dir = dirs::config_dir().unwrap().join("ross");
     std::fs::create_dir_all(&dir).unwrap();
+
+    #[cfg(target_os = "windows")]
+    set_hidden(&dir);
+
     dir
 }
 
-fn create_scheme_dir() -> PathBuf {
+pub fn create_scheme_dir() -> PathBuf {
     let dir = get_or_create_config_dir().join("schemes");
     std::fs::create_dir_all(&dir).unwrap();
     dir
 }
 
-fn get_config_path(file_name: &str) -> PathBuf {
+pub fn get_config_path(file_name: &str) -> PathBuf {
     get_or_create_config_dir().join(file_name)
+}
+
+#[cfg(target_os = "windows")]
+fn set_hidden(path: &std::path::Path) {
+    use std::os::windows::ffi::OsStrExt;
+
+    let wide: Vec<u16> = path.as_os_str().encode_wide().chain(std::iter::once(0)).collect();
+
+    unsafe {
+        let attrs = windows_sys::Win32::Storage::FileSystem::GetFileAttributesW(wide.as_ptr());
+        if attrs != u32::MAX {
+            windows_sys::Win32::Storage::FileSystem::SetFileAttributesW(
+                wide.as_ptr(),
+                attrs | windows_sys::Win32::Storage::FileSystem::FILE_ATTRIBUTE_HIDDEN,
+            );
+        }
+    }
 }
