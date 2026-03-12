ee901db9992874a5a931c1bd4fd35f2947f91fa8
Tomkov1c <tomkliner9@gmail.com>
2026-03-12 13:55:07 +0100

    Implement `CURRENT_DIR` from main

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,16 +1,14 @@
-use crate::handlers::{file_handler, local_config_handler};
+use crate::{CURRENT_DIR, handlers::{file_handler, local_config_handler}};
 use std::env;
 
 pub fn main(gitless: bool, gitignore: bool) {
     let success = local_config_handler::create_local_config_at_env();
 
     if !gitless {
-        let dir = env::current_dir().expect("Could not get current directory");
-
         if gitignore {
-            file_handler::add_config_dir_to_gitignore(dir);
+            file_handler::add_config_dir_to_gitignore(CURRENT_DIR.clone());
         } else {
-            file_handler::add_cache_to_gitignore(dir);
+            file_handler::add_cache_to_gitignore(CURRENT_DIR.clone());
         }
     }
 
