f85eb4637ddae4767f02a5cdad4f8831b179abab
Tomkov1c <tomkliner9@gmail.com>
2026-03-16 16:28:31 +0100

    Update `bob` command so it now actully outputs Bob Ross quotes

--- src/commands/bob.rs
+++ src/commands/bob.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 pub fn run() {
-    print!("Hello!");
+    let (quote, link) = match rand::random_range(0..10) {
+        0 => ( "Wiggle... wiggle wiggle", "https://youtu.be/VlucWfTUo1A?si=pW9MXse3NcBZzCjA&t=1311" ),
+        _ => ("Shake off the excess. *laughs* And just beat the devil out of it. This is where you take out all of your frustrations and hostilities and just have a good time.", "https://youtu.be/VlucWfTUo1A?si=DGVnKjoCq-DUSweD&t=256"
+        ),
+    };
+
+    let supports_links = env::var("TERM_PROGRAM").unwrap_or_default() != "Apple_Terminal";
+
+    if supports_links {
+        println!("̌\"{}\"", quote);
+        println!("  -- Bob Ross \x1b]8;;{}\x1b\\[Click to listen]\x1b]8;;\x1b\\", link);
+    } else {
+        println!("\"{}\"", quote);
+        println!("  -- Bob Ross [Listen here: {}]", link);
+    }
 }
