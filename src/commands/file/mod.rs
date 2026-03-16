178322503e2f1909b75ec433c25b3bf31264e191
Tomkov1c <tomkliner9@gmail.com>
2026-03-16 16:47:21 +0100

    Make `file` command accept files

--- src/commands/file/mod.rs
+++ src/commands/file/mod.rs
@@ -10,10 +10,17 @@ pub fn match_command(files: &[String], subcommand: Option<FileCommands>) {
     match subcommand {
         Some(FileCommands::Scheme { }) => { },
 
-        None => default(),
+        None => default(files),
     }
 }
 
-pub fn default() {
+pub fn default(files: &[String]) {
+    if files.is_empty() {
+        println!("No files provided.");
+        return;
+    }
 
+    for file in files {
+        println!("Processing file: {}", file);
+    }
 }
